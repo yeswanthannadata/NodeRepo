50398489916b65f63ecf1083a232cd669e48bbdc
yeswanthannadata <annadatayeswanth@gmail.com>
2021-08-15 15:03:45 +0530

    nodejs updated files

--- builtin-modules/fs-async-updated.js
+++ builtin-modules/fs-async-updated.js
@@ -1,15 +1,13 @@
-const { readFile, writeFile } = require("fs");
-const util = require("util");
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+const { readFile, writeFile } = require("fs").promises;
 
 const start = async () => {
   try {
-    const first = await readFilePromise("../content/first.txt", "utf8");
-    const second = await readFilePromise("../content/second.txt", "utf8");
-    await writeFilePromise(
+    const first = await readFile("../content/first.txt", "utf8");
+    const second = await readFile("../content/second.txt", "utf8");
+    await writeFile(
       "../content/result-using-promises.txt",
-      `THIS IS AWESOME: ${first}, ${second}`
+      `THIS IS AWESOME: ${first}, ${second}`,
+      { flag: "a" }
     );
     console.log(first, second);
   } catch (error) {
@@ -18,6 +16,12 @@ const start = async () => {
 };
 
 start();
+
+// old imports
+
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 // other usgae of promises using then
 
