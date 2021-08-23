0695fe0ca441ad90cf4245bc1cad284084048e91
yeswanthannadata <annadatayeswanth@gmail.com>
2021-08-23 23:50:41 +0530

    completion of nodejs course

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 
-const { products } = require("./data");
+const people = require("./routes/people");
+const auth = require("./routes/auth");
 
-app.get("/", (req, res) => {
-  res.json(products);
-});
+// static assets
+app.use(express.static("./methods-public"));
+
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+// parse json data
+app.use(express.json());
+
+// routes
+app.use("/api/people", people);
+app.use("/login", auth);
 
 app.listen(5000, () => {
   console.log("server listening on port 5000");
---
+++ node-express-course/02-express-tutorial/routes/people.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson,
+} = require("../controllers/people");
+
+// one kind of syntax
+
+// router.get("/", getPeople);
+// router.post("/", createPerson);
+// router.post("/postman", createPersonPostman);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
+
+// other chaining syntax
+
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
+
+module.exports = router;
