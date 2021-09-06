9cc7f5f810d9872522802dbd050b2513e2c74ca8
yeswanthannadata <annadatayeswanth@gmail.com>
2021-09-06 23:39:31 +0530

    task manager changes

--- node-express-course/03-task-manager/starter/app.js
+++ node-express-course/03-task-manager/starter/app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+app.use(express.json());
+// app.use(express.static("./public"));
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Hello Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`server is listening on port ${port}...`));
---
+++ node-express-course/03-task-manager/starter/db/connect.js
@@ -0,0 +1,2 @@
+const connectionString =
+  "mongodb+srv://yeswanth:<password>@nodeexpress.nfcbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
