e1952f2413f75c521439937978ead10e701207b2
yeswanthannadata <annadatayeswanth@gmail.com>
2021-09-13 09:50:59 +0530

    async and error middlewares

--- node-express-course/03-task-manager/starter/app.js
+++ node-express-course/03-task-manager/starter/app.js
@@ -4,6 +4,7 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middlewares/not-found");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
 
 // middleware
 app.use(express.static("./public"));
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
