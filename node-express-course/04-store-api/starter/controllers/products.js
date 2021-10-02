d9a453e4bc418f757784d58261054ab9dad37739
yeswanthannadata <annadatayeswanth@gmail.com>
2021-10-02 21:07:25 +0530

    added product API

--- node-express-course/04-store-api/starter/controllers/products.js
+++ node-express-course/04-store-api/starter/controllers/products.js
@@ -1,9 +1,35 @@
-const getAllProducts = (req, res) => {
-  res.status(200).json({ message: "All products" });
+const Product = require("../models/product");
+
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({
+    price: { $gt: 30 },
+  })
+    .sort("price")
+    .select("name price");
+
+  res.status(200).json({ products, nbHits: products.length });
 };
 
-const getAllProductsStatic = (req, res) => {
-  res.status(200).json({ message: "All Static products" });
+const getAllProductsStatic = async (req, res) => {
+  console.log(req.query);
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  const products = await Product.find(queryObject);
+
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
