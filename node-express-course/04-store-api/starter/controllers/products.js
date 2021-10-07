28cac1c34f7e6b62c3a9efeca25e09dba9f297a1
Yeshu <yeswanth@flutura.com>
2021-10-07 09:50:28 +0530

    page number and limit

--- node-express-course/04-store-api/starter/controllers/products.js
+++ node-express-course/04-store-api/starter/controllers/products.js
@@ -41,6 +41,12 @@ const getAllProductsStatic = async (req, res) => {
     result.select(fieldParam);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result.skip(skip).limit(limit);
+
   const products = await result;
 
   res.status(200).json({ products, nbHits: products.length });
