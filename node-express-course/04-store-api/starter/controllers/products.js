9ee44e85a0a848e11104fc5b5d1e6153b82764e0
Yeshu <yeswanth@flutura.com>
2021-10-07 17:44:41 +0530

    added numeric filters to the API

--- node-express-course/04-store-api/starter/controllers/products.js
+++ node-express-course/04-store-api/starter/controllers/products.js
@@ -11,8 +11,7 @@ const getAllProducts = async (req, res) => {
 };
 
 const getAllProductsStatic = async (req, res) => {
-  console.log(req.query);
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -25,6 +24,28 @@ const getAllProductsStatic = async (req, res) => {
 
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
 
   const result = Product.find(queryObject);
